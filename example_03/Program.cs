1a9e08f07ffaa6cc5ac6c4a6acf2cd88a324041d
«Rustam-Ledsager» <r.yakupov81@yandex.ru>
2022-09-10 14:21:46 +0300

    added example_03 ready

---
+++ example_03/Program.cs
@@ -0,0 +1,27 @@
+﻿int ElementInStroka(string strarray, char strcount)
+{
+    int count = 1;
+    foreach (var element in strarray)
+    {
+        if (strcount == element)
+            count++;
+    }
+    return count;
+}
+
+Console.WriteLine("Input text :");
+string text = Console.ReadLine();
+Console.WriteLine("enter the characters you want to analyze :");
+string searchsymboltext = Console.ReadLine();
+
+foreach (var element in searchsymboltext)
+{
+    int countelement = ElementInStroka(text, element);
+    int lengthtext = text.Length;
+    Console.WriteLine((double)countelement / lengthtext);
+    Console.WriteLine($"символ {element} встречается {ElementInStroka(text, element)} раз."
+    + $" Частота {(double)countelement / lengthtext}%");
+}
+Console.WriteLine(text.Length);
+
+
